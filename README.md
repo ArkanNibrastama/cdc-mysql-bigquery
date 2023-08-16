f01deefaa2093cabf0f12220b13a8f52cf928816
ArkanNibrastama <arkan6040nibrastama@gmail.com>
2023-08-16 14:28:48 +0700

    add docker-compose & API

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Change Data Capture from RDBMS (MySQL) to Data Warehouse (BigQuery)
+
---
+++ transactionAPI.py
@@ -0,0 +1,6 @@
+import flask
+from flask import Flask
+
+api = Flask('transactionAPI')
+
+# this file contain API for transaction of expedition 
