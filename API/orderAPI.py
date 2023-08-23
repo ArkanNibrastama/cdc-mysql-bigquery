f1e2355116821d61bfade6fdfeea7c166c23b880
ArkanNibrastama <arkan6040nibrastama@gmail.com>
2023-08-23 21:41:24 +0700

    update api

--- API/orderAPI.py
+++ API/orderAPI.py
@@ -14,6 +14,9 @@ class OrderBase(BaseModel):
     order_date: str
     order_status: str
 
+class UpdateStatusBase(BaseModel):
+    order_status: str
+
 # database dependencies
 def get_db():
     db = SessionLocal()
@@ -25,9 +28,18 @@ def get_db():
 db_dependency = Annotated[Session, Depends(get_db)]
 
 # endpoint
+# add a new order transaction
 @api.post("/order/", status_code=status.HTTP_201_CREATED)
 async def create_order(order:OrderBase, db:db_dependency):
-    db_user = models.Order(**order.dict())
-    db.add(db_user)
+    input_order = models.Order(**order.dict())
+    db.add(input_order)
     db.commit()
 
+# update partial (just the status)
+@api.patch("/status/{order_id}", status_code=status.HTTP_200_OK)
+async def update_status(order_id:int, status:UpdateStatusBase, db:db_dependency):
+    old_status = db.query(models.Order).filter(models.Order.order_id == order_id).first()
+    for var, value in vars(status).items():
+        setattr(old_status, var, value) if value else None
+    db.add(old_status)
+    db.commit()
